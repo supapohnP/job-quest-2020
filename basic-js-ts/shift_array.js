fef06cdf3c048e3ceb6d77280ea7a4523305b189
Supapohn Pukeaw <p.supapohn@gmail.com>
2020-10-24 12:44:09 +0700

    add file shift_array.js

---
+++ basic-js-ts/shift_array.js
@@ -0,0 +1,15 @@
+function shift (arr, set, n) {
+    const i = arr.length - n;
+    if (set == 'left') {
+        const front = arr.slice(0, i);
+        const back = arr.slice(i);
+        return back.concat(front);
+    } if (set == 'right') {
+        const back = arr.slice(0, i);
+        const front = arr.slice(i);
+        return front.concat(back);
+    } 
+}
+
+console.log(shift(['john','jane','sarah','alex'], 'left',2));
+console.log(shift([1,2,3,4,5], 'right',3));
